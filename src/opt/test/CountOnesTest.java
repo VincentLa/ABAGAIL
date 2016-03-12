a822f0182db02148c0bee9c098498b32840dedd5
Vincent La <vincela14@gmail.com>
2016-03-12 09:21:32 -0800

    Adding some changes

--- src/opt/test/CountOnesTest.java
+++ src/opt/test/CountOnesTest.java
@@ -26,6 +26,10 @@ import opt.prob.MIMIC;
 import opt.prob.ProbabilisticOptimizationProblem;
 import shared.FixedIterationTrainer;
 
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
 /**
  * 
  * @author Andrew Guillory gtg008g@mail.gatech.edu
@@ -36,36 +40,156 @@ public class CountOnesTest {
     private static final int N = 80;
     
     public static void main(String[] args) {
-        int[] ranges = new int[N];
-        Arrays.fill(ranges, 2);
-        EvaluationFunction ef = new CountOnesEvaluationFunction();
-        Distribution odd = new DiscreteUniformDistribution(ranges);
-        NeighborFunction nf = new DiscreteChangeOneNeighbor(ranges);
-        MutationFunction mf = new DiscreteChangeOneMutation(ranges);
-        CrossoverFunction cf = new UniformCrossOver();
-        Distribution df = new DiscreteDependencyTree(.1, ranges); 
-        HillClimbingProblem hcp = new GenericHillClimbingProblem(ef, odd, nf);
-        GeneticAlgorithmProblem gap = new GenericGeneticAlgorithmProblem(ef, odd, mf, cf);
-        ProbabilisticOptimizationProblem pop = new GenericProbabilisticOptimizationProblem(ef, odd, df);
-        
-        RandomizedHillClimbing rhc = new RandomizedHillClimbing(hcp);      
-        FixedIterationTrainer fit = new FixedIterationTrainer(rhc, 200);
-        fit.train();
-        System.out.println(ef.value(rhc.getOptimal()));
-        
-        SimulatedAnnealing sa = new SimulatedAnnealing(100, .95, hcp);
-        fit = new FixedIterationTrainer(sa, 200);
-        fit.train();
-        System.out.println(ef.value(sa.getOptimal()));
-        
-        StandardGeneticAlgorithm ga = new StandardGeneticAlgorithm(20, 20, 0, gap);
-        fit = new FixedIterationTrainer(ga, 300);
-        fit.train();
-        System.out.println(ef.value(ga.getOptimal()));
-        
-        MIMIC mimic = new MIMIC(50, 10, pop);
-        fit = new FixedIterationTrainer(mimic, 100);
-        fit.train();
-        System.out.println(ef.value(mimic.getOptimal()));
+        List<Double> rhctimeList = new ArrayList<Double>();
+        List<Double> satimeList = new ArrayList<Double>();
+        List<Double> gatimeList = new ArrayList<Double>();
+        List<Double> mimictimeList = new ArrayList<Double>();
+        List<Double> rhcList = new ArrayList<Double>();
+        List<Double> saList = new ArrayList<Double>();
+        List<Double> gaList = new ArrayList<Double>();
+        List<Double> mimicList = new ArrayList<Double>();
+        for(int i = 0; i < 10; i++) {
+            int[] ranges = new int[N];
+            Arrays.fill(ranges, 2);
+            EvaluationFunction ef = new CountOnesEvaluationFunction();
+            Distribution odd = new DiscreteUniformDistribution(ranges);
+            NeighborFunction nf = new DiscreteChangeOneNeighbor(ranges);
+            MutationFunction mf = new DiscreteChangeOneMutation(ranges);
+            CrossoverFunction cf = new UniformCrossOver();
+            Distribution df = new DiscreteDependencyTree(.1, ranges); 
+            HillClimbingProblem hcp = new GenericHillClimbingProblem(ef, odd, nf);
+            GeneticAlgorithmProblem gap = new GenericGeneticAlgorithmProblem(ef, odd, mf, cf);
+            ProbabilisticOptimizationProblem pop = new GenericProbabilisticOptimizationProblem(ef, odd, df);
+            
+            RandomizedHillClimbing rhc = new RandomizedHillClimbing(hcp);      
+            FixedIterationTrainer fit = new FixedIterationTrainer(rhc, 200);
+            double start = System.nanoTime(), end, trainingTime;
+            fit.train();
+            end = System.nanoTime();
+            trainingTime = end - start;
+            trainingTime /= Math.pow(10,9);
+            rhctimeList.add(trainingTime);
+            rhcList.add(ef.value(rhc.getOptimal()));
+            /*System.out.println(ef.value(rhc.getOptimal()));*/
+            
+            SimulatedAnnealing sa = new SimulatedAnnealing(100, .95, hcp);
+            fit = new FixedIterationTrainer(sa, 200);
+            start = System.nanoTime();
+            fit.train();
+            end = System.nanoTime();
+            trainingTime = end - start;
+            trainingTime /= Math.pow(10,9);
+            satimeList.add(trainingTime);
+            saList.add(ef.value(sa.getOptimal()));
+            /*System.out.println(ef.value(sa.getOptimal()));*/
+            
+            StandardGeneticAlgorithm ga = new StandardGeneticAlgorithm(20, 20, 0, gap);
+            fit = new FixedIterationTrainer(ga, 300);
+            start = System.nanoTime();
+            fit.train();
+            end = System.nanoTime();
+            trainingTime = end - start;
+            trainingTime /= Math.pow(10,9);
+            gatimeList.add(trainingTime);
+            /*System.out.println(ef.value(ga.getOptimal()));*/
+            gaList.add(ef.value(ga.getOptimal()));
+            
+            MIMIC mimic = new MIMIC(50, 10, pop);
+            fit = new FixedIterationTrainer(mimic, 100);
+            start = System.nanoTime();
+            fit.train();
+            end = System.nanoTime();
+            trainingTime = end - start;
+            trainingTime /= Math.pow(10,9);
+            mimictimeList.add(trainingTime);
+            /*System.out.println(ef.value(mimic.getOptimal()));*/
+            mimicList.add(ef.value(mimic.getOptimal()));
+        }
+
+        /*Average Value RHC*/
+        Double sum = 0d;
+        Double avg_value =0d;
+        for (Double vals : rhcList) {
+            sum += vals;
+        }
+         avg_value =  sum / rhcList.size();
+         System.out.println("RHC");
+         System.out.println(avg_value);
+
+        /*Average Value SA*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : saList) {
+            sum += vals;
+        }
+         avg_value =  sum / saList.size();
+         System.out.println("SA");
+         System.out.println(avg_value);
+
+        /*Average Value GA*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : gaList) {
+            sum += vals;
+        }
+         avg_value =  sum / gaList.size();
+         System.out.println("GA");
+         System.out.println(avg_value);
+
+        /*Average Value MIMIC*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : mimicList) {
+            sum += vals;
+        }
+         avg_value =  sum / mimicList.size();
+         System.out.println("MIMIC");
+         System.out.println(avg_value);
+
+/*
+*
+*Training time comparison
+*
+*/
+
+        /*Average Value RHC*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : rhctimeList) {
+            sum += vals;
+        }
+         avg_value =  sum / rhctimeList.size();
+         System.out.println("RHC");
+         System.out.println(avg_value);
+
+        /*Average Value SA*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : satimeList) {
+            sum += vals;
+        }
+         avg_value =  sum / satimeList.size();
+         System.out.println("SA");
+         System.out.println(avg_value);
+
+        /*Average Value GA*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : gatimeList) {
+            sum += vals;
+        }
+         avg_value =  sum / gatimeList.size();
+         System.out.println("GA");
+         System.out.println(avg_value);
+
+        /*Average Value MIMIC*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : mimictimeList) {
+            sum += vals;
+        }
+         avg_value =  sum / mimictimeList.size();
+         System.out.println("MIMIC");
+         System.out.println(avg_value);
     }
 }
--- src/opt/test/FourPeaksTest.java
+++ src/opt/test/FourPeaksTest.java
@@ -26,47 +26,175 @@ import opt.prob.MIMIC;
 import opt.prob.ProbabilisticOptimizationProblem;
 import shared.FixedIterationTrainer;
 
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+
 /**
  * Copied from ContinuousPeaksTest
  * @version 1.0
  */
 public class FourPeaksTest {
     /** The n value */
-    private static final int N = 200;
+    private static final int N = 20;
     /** The t value */
     private static final int T = N / 5;
     
     public static void main(String[] args) {
-        int[] ranges = new int[N];
-        Arrays.fill(ranges, 2);
-        EvaluationFunction ef = new FourPeaksEvaluationFunction(T);
-        Distribution odd = new DiscreteUniformDistribution(ranges);
-        NeighborFunction nf = new DiscreteChangeOneNeighbor(ranges);
-        MutationFunction mf = new DiscreteChangeOneMutation(ranges);
-        CrossoverFunction cf = new SingleCrossOver();
-        Distribution df = new DiscreteDependencyTree(.1, ranges); 
-        HillClimbingProblem hcp = new GenericHillClimbingProblem(ef, odd, nf);
-        GeneticAlgorithmProblem gap = new GenericGeneticAlgorithmProblem(ef, odd, mf, cf);
-        ProbabilisticOptimizationProblem pop = new GenericProbabilisticOptimizationProblem(ef, odd, df);
-        
-        RandomizedHillClimbing rhc = new RandomizedHillClimbing(hcp);      
-        FixedIterationTrainer fit = new FixedIterationTrainer(rhc, 200000);
-        fit.train();
-        System.out.println("RHC: " + ef.value(rhc.getOptimal()));
-        
-        SimulatedAnnealing sa = new SimulatedAnnealing(1E11, .95, hcp);
-        fit = new FixedIterationTrainer(sa, 200000);
-        fit.train();
-        System.out.println("SA: " + ef.value(sa.getOptimal()));
-        
-        StandardGeneticAlgorithm ga = new StandardGeneticAlgorithm(200, 100, 10, gap);
-        fit = new FixedIterationTrainer(ga, 1000);
-        fit.train();
-        System.out.println("GA: " + ef.value(ga.getOptimal()));
-        
-        MIMIC mimic = new MIMIC(200, 20, pop);
-        fit = new FixedIterationTrainer(mimic, 1000);
-        fit.train();
-        System.out.println("MIMIC: " + ef.value(mimic.getOptimal()));
+        List<Double> rhctimeList = new ArrayList<Double>();
+        List<Double> satimeList = new ArrayList<Double>();
+        List<Double> gatimeList = new ArrayList<Double>();
+        List<Double> mimictimeList = new ArrayList<Double>();
+        List<Double> rhcList = new ArrayList<Double>();
+        List<Double> saList = new ArrayList<Double>();
+        List<Double> gaList = new ArrayList<Double>();
+        List<Double> mimicList = new ArrayList<Double>();
+
+        for(int i = 0; i < 10; i++) {        
+            int[] ranges = new int[N];
+            Arrays.fill(ranges, 2);
+            EvaluationFunction ef = new FourPeaksEvaluationFunction(T);
+            Distribution odd = new DiscreteUniformDistribution(ranges);
+            NeighborFunction nf = new DiscreteChangeOneNeighbor(ranges);
+            MutationFunction mf = new DiscreteChangeOneMutation(ranges);
+            CrossoverFunction cf = new SingleCrossOver();
+            Distribution df = new DiscreteDependencyTree(.1, ranges); 
+            HillClimbingProblem hcp = new GenericHillClimbingProblem(ef, odd, nf);
+            GeneticAlgorithmProblem gap = new GenericGeneticAlgorithmProblem(ef, odd, mf, cf);
+            ProbabilisticOptimizationProblem pop = new GenericProbabilisticOptimizationProblem(ef, odd, df);
+            
+            RandomizedHillClimbing rhc = new RandomizedHillClimbing(hcp);      
+            FixedIterationTrainer fit = new FixedIterationTrainer(rhc, 200000);
+            double start = System.nanoTime(), end, trainingTime;
+            fit.train();
+            end = System.nanoTime();
+            trainingTime = end - start;
+            trainingTime /= Math.pow(10,9);
+            rhctimeList.add(trainingTime);
+            rhcList.add(ef.value(rhc.getOptimal()));
+            
+            /*System.out.println("RHC: " + ef.value(rhc.getOptimal()));*/
+            
+            SimulatedAnnealing sa = new SimulatedAnnealing(1E11, .95, hcp);
+            fit = new FixedIterationTrainer(sa, 200000);
+            start = System.nanoTime();
+            fit.train();
+            end = System.nanoTime();
+            trainingTime = end - start;
+            trainingTime /= Math.pow(10,9);
+            satimeList.add(trainingTime);
+            saList.add(ef.value(sa.getOptimal()));
+
+            /*System.out.println("SA: " + ef.value(sa.getOptimal()));*/
+            
+            StandardGeneticAlgorithm ga = new StandardGeneticAlgorithm(200, 100, 10, gap);
+            fit = new FixedIterationTrainer(ga, 1000);
+            start = System.nanoTime();
+            fit.train();
+            end = System.nanoTime();
+            trainingTime = end - start;
+            trainingTime /= Math.pow(10,9);
+            gatimeList.add(trainingTime);
+            gaList.add(ef.value(ga.getOptimal()));
+            /*System.out.println("GA: " + ef.value(ga.getOptimal()));*/
+            
+            MIMIC mimic = new MIMIC(200, 20, pop);
+            fit = new FixedIterationTrainer(mimic, 1000);
+            start = System.nanoTime();           
+            fit.train();
+            end = System.nanoTime();
+            trainingTime = end - start;
+            trainingTime /= Math.pow(10,9);
+            mimictimeList.add(trainingTime);
+            mimicList.add(ef.value(mimic.getOptimal()));
+            /*System.out.println("MIMIC: " + ef.value(mimic.getOptimal()));*/
+        }
+
+        /*Average Value RHC*/
+        Double sum = 0d;
+        Double avg_value =0d;
+        for (Double vals : rhcList) {
+            sum += vals;
+        }
+         avg_value =  sum / rhcList.size();
+         System.out.println("RHC");
+         System.out.println(avg_value);
+
+        /*Average Value SA*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : saList) {
+            sum += vals;
+        }
+         avg_value =  sum / saList.size();
+         System.out.println("SA");
+         System.out.println(avg_value);
+
+        /*Average Value GA*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : gaList) {
+            sum += vals;
+        }
+         avg_value =  sum / gaList.size();
+         System.out.println("GA");
+         System.out.println(avg_value);
+
+        /*Average Value MIMIC*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : mimicList) {
+            sum += vals;
+        }
+         avg_value =  sum / mimicList.size();
+         System.out.println("MIMIC");
+         System.out.println(avg_value);
+
+/*
+*
+*Training time comparison
+*
+*/
+
+        /*Average Value RHC*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : rhctimeList) {
+            sum += vals;
+        }
+         avg_value =  sum / rhctimeList.size();
+         System.out.println("RHC");
+         System.out.println(avg_value);
+
+        /*Average Value SA*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : satimeList) {
+            sum += vals;
+        }
+         avg_value =  sum / satimeList.size();
+         System.out.println("SA");
+         System.out.println(avg_value);
+
+        /*Average Value GA*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : gatimeList) {
+            sum += vals;
+        }
+         avg_value =  sum / gatimeList.size();
+         System.out.println("GA");
+         System.out.println(avg_value);
+
+        /*Average Value MIMIC*/
+        sum = 0d;
+        avg_value =0d;
+        for (Double vals : mimictimeList) {
+            sum += vals;
+        }
+         avg_value =  sum / mimictimeList.size();
+         System.out.println("MIMIC");
+         System.out.println(avg_value);
     }
 }
